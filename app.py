a12eb188e34256099af46130148beee61e3e9d16
SedatCeyhan <sedat.sabancidx@gmail.com>
2022-04-16 12:30:44 +0100

    trial1

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def create_availability():
         # Before adding the availability, make sure to flag the availabbility as 'unscheduled'!
         athlete_availability['isScheduled'] = False
         # Sharding based on region_code (continent)
-        db_addition = db[region_code + "-athletes"].insert_one(athlete_availability)
+        db_addition = db[region_code + "-athletes"].insert_one({"Conor":"Church"})
         
         return make_response(jsonify(athlete_availability), 200)
     
