5b3b35d35538ddbe5183ac6b00371fba2a4a48d6
Conor Church <conorchurch42@gmail.com>
2022-04-17 15:17:27 +0100

    Editing script for less testers and different emails for testers

--- tester_data.py
+++ tester_data.py
@@ -4,7 +4,7 @@ import pymongo
 CONNECTION_STRING = "mongodb://group6api:YXeNbRRXMlCbDuHPyCG10sQARbUllgR4tmazMeDu946ZagcBAWlHu9qAXCoRrjOiuFCaR8glsmYr5CM1v42RLg==@group6api.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@group6api@"
 DB_NAME = "EuropeDB"
 
-def push_random_collection():
+def populate_tester_collection():
 
     db = client = pymongo.MongoClient(CONNECTION_STRING)
     db = client[DB_NAME]
@@ -12,19 +12,21 @@ def push_random_collection():
     collection_prefix = ["NA", "EU", "AU", "AS"]
     regions = ["North America", "Europe", "Australia", "Asia"]
     countries = ["America", "Ireland", "Australia", "China"]
+    random_names = ["conor", "ankana", "yuki", "aryan", "sedat", "john", "mark", "paul"]
 
     i = 0
-    while i < 100: 
+    while i < 50: 
 
         random_num = randint(0,3)
-        data = {"tester_email": "conor", "region": regions[random_num], "country": countries[random_num]}
+        random_name = randint(0,7)
+        data = {"tester_email": random_names[random_name]+random_num+"@gmail.com", "region": regions[random_num], "country": countries[random_num]}
         db[collection_prefix[random_num]+"-testers"].insert_one(data).inserted_id
         i+=1
 
 
 
 def main():
-    push_random_collection()
+    populate_tester_collection()
 
 
 if __name__ == '__main__':
