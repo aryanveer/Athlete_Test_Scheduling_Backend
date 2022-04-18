16a2d3fa6582a468a98fe57882e5c7da9c4c30e4
SedatCeyhan <sedat.sabancidx@gmail.com>
2022-04-18 02:16:56 +0100

    [add] schedule by country and get tester schedules

---
+++ helper_country.py
@@ -0,0 +1,15 @@
+countries_to_continent = {
+    'France' : "EU", 
+    'Ireland': "EU", 
+    'England': "EU",
+    
+    'Canada' : 'NA', 
+    'America' :'NA',
+    
+    'China' : 'AS', 
+    'India' : 'AS', 
+    'Japan' : 'AS',
+    
+    'Australia' : 'AU'
+    
+}
